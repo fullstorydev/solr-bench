2a53a93e41bb7e21212a397eb05d852eec501ccc
Shan He <shan@fullstory.com>
2020-09-10 11:38:22 -0700

    Revert "write response to file."
    This reverts commit c89a2a2ab42425d192169ee076c0191b3a0636b2.

--- src/main/java/org/apache/solr/benchmarks/BenchmarksMain.java
+++ src/main/java/org/apache/solr/benchmarks/BenchmarksMain.java
@@ -1,10 +1,18 @@
 package org.apache.solr.benchmarks;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
 import java.lang.invoke.MethodHandles;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -186,16 +194,6 @@ public class BenchmarksMain {
             e.printStackTrace();
         } finally {
             solrCloud.shutdown(true);
-            RESULT_DUMP.close();
-        }
-    }
-
-    static volatile BufferedWriter RESULT_DUMP;
-    static {
-        try {
-            RESULT_DUMP = Files.newBufferedWriter(Paths.get("dump-result-" + System.currentTimeMillis() + ".txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
@@ -221,10 +219,6 @@ public class BenchmarksMain {
         }
         IOUtils.copy((InputStream) rsp.get("stream"), baos);
         String errorout = new String(baos.toByteArray());
-        synchronized(BenchmarksMain.class) {
-            RESULT_DUMP.write(errorout);
-            RESULT_DUMP.newLine();
-        }
         if (!errorout.trim().startsWith("{")) {
             // it's not a JSON output, something must be wrong
             System.out.println("########### A query failed ");
