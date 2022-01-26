2285d114093357ef2fe9ba0bcd11c85f485552ea
Patson Luk <patson@fullstory.com>
2022-01-26 11:31:46 -0800

    Mark outputs.tf with sensitive=true for TF checks

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,11 @@
 output "zookeeper_details" {
   description = "Details of the Zookeeper machine"
   value       = module.zookeeper.instance_details
+  sensitive = true
 }
 
 output "solr_node_details" {
   description = "Details of the solr machines"
   value       = module.solr_node.instance_details
+  sensitive = true
 }
