73b207bb2fa51b3f640a277c304e0b4c0e48e3d8
Justin Sweeney <justinsweeney@fullstory.com>
2022-10-12 16:27:36 -0400

    Removing TF changes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,9 @@
 output "zookeeper_details" {
-  sensitive = true
   description = "Details of the Zookeeper machine"
   value       = module.zookeeper.instance_details
 }
 
 output "solr_node_details" {
-  sensitive = true
   description = "Details of the solr machines"
   value       = module.solr_node.instance_details
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = "~>0.12.0"
   required_providers {
-    google = "~> 4.39.0"
+    google = "~> 3.20.0"
   }
 }
