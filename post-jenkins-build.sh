c45ff956abad233295b90038e983880f0b116018
Ishan Chattopadhyaya <ishan@apache.org>
2022-11-07 14:45:28 +0530

    Publish graph on Jenkins

--- post-jenkins-build.sh
+++ post-jenkins-build.sh
@@ -1,2 +1,6 @@
+cd /home/ishan/code/solr-bench
+python createGraph.py
+cp cluster-test.json.html /var/www/html 
+
 BASEDIR=$(realpath $(dirname "$0"))
 rm $BASEDIR/SolrNightlyBenchmarksWorkDirectory/Download/solr-*.tgz
