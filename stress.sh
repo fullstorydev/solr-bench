dd1d2a18e4e7de9583be5fdb6a78a8b89ab97374
patsonluk <patson@fullstory.com>
2023-01-10 15:07:40 -0800

    Do not pre-download apache zk for provisioning-method == existing

--- stress.sh
+++ stress.sh
@@ -97,7 +97,11 @@ terraform-gcp-provisioner() {
 
 # Download the pre-requisites
 download `jq -r '."cluster"."jdk-url"' $CONFIGFILE`
-wget -c https://downloads.apache.org/zookeeper/zookeeper-3.6.3/apache-zookeeper-3.6.3-bin.tar.gz 
+
+if [ "existing" != `jq -r '.["cluster"]["provisioning-method"]' $CONFIGFILE` ]
+then
+  wget -c https://downloads.apache.org/zookeeper/zookeeper-3.6.3/apache-zookeeper-3.6.3-bin.tar.gz
+fi
 for i in `jq -r '."pre-download" | .[]' $CONFIGFILE`; do download $i; done
 
 # Clone/checkout the git repository and build Solr
