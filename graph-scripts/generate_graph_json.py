2ded53d960d1fe14e0153ee630966e327a255765
patsonluk <patsonluk@users.noreply.github.com>
2023-01-28 13:19:39 -0800

    Handle graph rendering logic in js. Added paging and comparisons between branches (#38)
    * generate json file instead of html
    
    * attempt to use graph.js to generate graph instead of from python

---
+++ graph-scripts/generate_graph_json.py
@@ -0,0 +1,166 @@
+import copy
+
+import json
+import time
+import os
+import math
+import collections
+import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO)
+parser = argparse.ArgumentParser(description='Description of your program')
+parser.add_argument('-r', '--result-dir', help='Directory that contains the json result files', required=True)
+parser.add_argument('-o', '--output',
+                    help='Output path of the graph json. If undefined it will be saved as the working dir with name '
+                         '<test_name>.json ',
+                    required=False)
+parser.add_argument('-b', '--branches',
+                    help='Result for a single branch <branch> or compare branches in format of <branch1>...<branch2>',
+                    required=True)
+args = vars(parser.parse_args())
+
+result_dir = args['result_dir']
+logging.info("Reading results from dir: " + result_dir)
+target_branches = None
+if args.get("branches") is not None:
+    target_branches = args['branches'].split('...')
+    logging.info("Comparing branches: " + str(target_branches))
+
+
+def load_properties(filepath, sep='=', comment_char='#'):
+    """
+    Read the file passed as parameter as a properties file.
+    """
+    props = {}
+    with open(filepath, "rt") as f:
+        for line in f:
+            l = line.strip()
+            if l and not l.startswith(comment_char):
+                key_value = l.split(sep)
+                key = key_value[0].strip()
+                value = sep.join(key_value[1:]).strip().strip('"')
+                props[key] = value
+    return props
+
+
+element_keys = []
+
+
+def get_element_id(key):
+    if key not in element_keys:
+        element_keys.append(key)
+    return f'element_{element_keys.index(key)}'
+
+
+class BenchmarkResult:
+    def __init__(self, branch, commit_hash, commit_date, commit_msg, results):
+        self.branch = branch
+        self.commit_hash = commit_hash
+        self.commit_date = commit_date
+        self.commit_msg = commit_msg
+        self.results = results
+
+    # def add_timing(self, key, timing):
+    #     self.task_timing[key] = timing
+
+    def __str__(self):
+        return "Branch: %s Hash: %s Commit Date: %s Commit Msg: %s Results: %s" % (
+        self.branch, self.commit_hash, self.commit_date, self.commit_msg, str(self.results))
+
+    def __repr__(self):
+        return str(self)
+
+
+def parse_benchmark_results(result_paths):
+    benchmark_results = []
+    for result_path in result_paths:
+        result_dir = os.path.dirname(result_path)
+        commit_hash = os.path.basename(result_path)[len("results-"):-1 * len(".json")]
+        logging.info("Hash: " + commit_hash)
+        meta_path = os.path.join(result_dir, "meta-" + commit_hash + ".prop")
+        logging.info("Meta file: " + meta_path)
+        props = load_properties(meta_path)
+        logging.info("Loaded props" + str(props))
+
+        # commit_date = time.gmtime(int(props["committed_date"]))
+        commit_date = int(props["committed_date"])
+        commit_msg = props["message"]
+
+        try:
+            json_results = json.load(open(result_path))
+        except OSError as e:
+            logging.warning(f"Skipping {commit_hash}. Unable to open {result_path}: {e}")
+            continue
+
+        benchmark_result = BenchmarkResult(branch, commit_hash, commit_date, commit_msg, json_results)
+        benchmark_results.append(benchmark_result.__dict__)
+
+    return benchmark_results
+
+
+def get_committed_date(props):
+    return int(props["committed_date"])
+
+
+class BranchTaskKey:
+    def __init__(self, branch, task_key):
+        self.branch = branch
+        self.task_key = task_key
+
+    def __eq__(self, other):
+        return (self.branch, self.task_key) == (other.branch, other.task_key)
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __lt__(self, other):
+        return (self.branch, self.task_key) < (other.branch, other.task_key)
+
+    def __repr__(self):
+        return f"{self.task_key}({self.branch})"
+
+    def __hash__(self):
+        # necessary for instances to behave sanely in dicts and sets.
+        return hash((self.branch, self.task_key))
+
+
+
+branches = []
+benchmark_results = collections.OrderedDict()  # key as branch name
+test_name = os.path.splitext(os.path.basename(result_dir))[0]
+
+meta_files = [f for f in os.listdir(result_dir) if
+              os.path.isfile(os.path.join(result_dir, f)) and f.startswith('meta-')]
+
+for branch in target_branches:
+    meta_files = [f for f in os.listdir(result_dir) if
+                  os.path.isfile(os.path.join(result_dir, f)) and f.startswith('meta-')]
+    meta_props = []
+    result_paths = []
+    for meta_file in meta_files:
+        props = load_properties(os.path.join(result_dir, meta_file))
+        if branch not in props["branches"].split(','):
+            logging.debug(f'skipping {meta_file} for branch {branch}')
+            continue
+        commit_hash = meta_file[len("meta-"):-1 * len(".json")]
+        props["hash"] = commit_hash
+        meta_props.append(props)
+
+    # now sort the props by commit date
+    meta_props.sort(key=get_committed_date)
+    for props in meta_props:
+        result_paths.append(os.path.join(result_dir, f'results-{props["hash"]}.json'))
+
+    benchmark_results[branch] = parse_benchmark_results(result_paths)
+
+
+output_path = None
+if args.get("output") is not None:
+    output_path = args['output']
+else:
+    output_path = "graph/graph-data.js"
+with open(output_path, "w") as output_file:
+    output_file.write("var graph_data=" + json.dumps(benchmark_results, indent=4))
+
+logging.info(f'Processed results {output_path} generated')
---
+++ graph/graph.js
@@ -0,0 +1,245 @@
+function drawAllCharts() {
+    var allResultsByTaskName = {}
+    var branches = []
+    $.each(graph_data, function(branch, branchData) { //collect branch names first. then figure out how many pages are there
+       branches.push(branch)
+    })
+
+    $.each(graph_data, function(branch, branchData) {
+        var $page = generatePage(branch, graph_data.length == 1)
+
+        var branchResultsByTaskName = {}
+        $.each(branchData, function(index, resultByCommit) {
+            var commitMeta = {
+                branch: resultByCommit.branch,
+                commitHash: resultByCommit.commit_hash,
+                commitDate: resultByCommit.commit_date,
+                commitMsg: resultByCommit.commit_msg
+            }
+            $.each(resultByCommit.results, function(taskName, resultByTask) {
+                var resultsOfThisTask = branchResultsByTaskName[taskName]
+                if (!resultsOfThisTask) {
+                    resultsOfThisTask = []
+                    branchResultsByTaskName[taskName] = resultsOfThisTask
+                }
+                var entry = {
+                    commitMeta : commitMeta,
+                    result: resultByTask[0]
+                }
+                resultsOfThisTask.push(entry)
+            })
+        })
+        //plot graph of this branch of this task
+        $.each(branchResultsByTaskName, function(taskName, resultsByTask) {
+            drawChartInPage([branch], taskName, resultsByTask, $page)
+            if (!allResultsByTaskName[taskName]) {
+                allResultsByTaskName[taskName] = [].concat(resultsByTask)
+            } else {
+                allResultsByTaskName[taskName].push(...resultsByTask)
+            }
+        })
+    })
+
+    //generate a graph with all branches
+    if (branches.length > 1) {
+        var $page = generatePage(branches.join(' vs '), true)
+        $.each(allResultsByTaskName, function(taskName, resultsByTask) {
+            drawChartInPage(branches, taskName, resultsByTask, $page)
+        })
+        //have to refresh here, if the page is hidden on render then the graph dimension is rendered incorrectly...
+        $page.siblings('.page').hide()
+    }
+}
+
+const ChartTypes = {
+	Simple: Symbol("Simple"),
+	Percentile: Symbol("Percentile")
+}
+
+function generatePage(title, selected) {
+    var $page = $('<div class="page"></div>').appendTo($('#mainContainer'))
+
+    var $item = $('<div class="item clickable"><p>' + title + '</p></div>')
+    if (selected) {
+        $item.addClass('selected')
+    }
+    $item.click(function() {
+        $(this).siblings().removeClass('selected')
+        $(this).addClass('selected')
+        $page.siblings('.page').hide()
+        $page.show()
+    })
+    $('#sideBar').append($item)
+    return $page
+}
+
+function detectChartType(graphData) {
+    return graphData['result']['timings'] ? ChartTypes.Percentile : ChartTypes.Simple
+}
+
+var graphIndex = 0
+function drawChartInPage(branches, taskName, graphDataByCommit, $page) {
+    var elementId = 'graph_' + graphIndex++
+    var $graphDiv = $('<div class="graph" id="' + elementId + '"></div>')
+    $page.append($graphDiv)
+//    $('#canvas').append($graphDiv)
+
+//TODO xaxis, yaxis
+    var title = taskName + ' (' + branches.join(' vs ') + ')'
+    var options = {
+                    title: title,
+                    hAxis: {
+                        title: 'Commit date',
+                        titleTextStyle: {
+                        color: '#333'
+                        },
+                        //slantedText: true,
+                        //slantedTextAngle: 80
+                    },
+                    vAxis: {
+                        title: 'Time (seconds)',
+                        minValue: 0
+                    },
+                    explorer: {
+                        actions: ['dragToZoom', 'rightClickToReset'],
+                        axis: 'horizontal',
+                        keepInBounds: true,
+                        maxZoomIn: 4.0
+                    },
+                    chartArea: {width: '70%'},
+                };
+
+    var data = new google.visualization.DataTable();
+    var chartType = detectChartType(graphDataByCommit[0])
+    var columns = []
+
+    columns.push({type: 'date', id:'Commit date'})
+    $.each(branches, function(index, branch) {
+        var suffix = ''
+        if (branches.length > 1) {
+            suffix = ' (' + branch + ')'
+        }
+        if (chartType === ChartTypes.Simple) {
+            columns.push({type: 'number', label: "duration" + suffix})
+            columns.push({type: 'string', role:'tooltip'})
+        } else if (chartType === ChartTypes.Percentile) {
+            columns.push({type: 'number', label: "median" + suffix})
+            columns.push({type: 'string', role:'tooltip'})
+            columns.push({type: 'number', label: "p90" + suffix})
+            columns.push({type: 'string', role:'tooltip'})
+            columns.push({type: 'number', label: "p95" + suffix})
+            columns.push({type: 'string', role:'tooltip'})
+            columns.push({type: 'number', label: "mean" + suffix})
+            columns.push({type: 'string', role:'tooltip'})
+        }
+    })
+
+    //view column is used to control which column is visible, it's simply an array of either integer if visible (with value as the column index)
+    //or a json object with calc function that returns null, this is super weird, but see
+    //https://stackoverflow.com/questions/17444586/show-hide-lines-data-in-google-chart
+    var viewColumns = []
+    $.each(columns, function(index, column) {
+        if (column.type === 'number') {
+            column.visible = true //this property is NOT used by google chart, is it simply for us to track the column state
+        }
+        viewColumns.push(index)
+        data.addColumn(column)
+    })
+
+
+    var rows = []
+    $.each(graphDataByCommit, function(index, dataOfCommit) {
+        var row = []
+        var commitDate = new Date(0)
+        commitDate.setUTCSeconds(dataOfCommit.commitMeta.commitDate);
+        row.push(commitDate)
+        $.each(branches, function(walkerBranchIndex, branch) { //walk through branch and pad if necessary
+            if (branch == dataOfCommit.commitMeta.branch) {
+                if (chartType === ChartTypes.Simple) {
+                    var duration = dataOfCommit['result']['end-time'] - dataOfCommit['result']['start-time']
+                    row.push(duration)
+                    row.push(duration + ' (' + branch + ') ' + dataOfCommit.commitMeta.commitMsg)
+                } else if (chartType === ChartTypes.Percentile) {
+                    var timingResult = dataOfCommit['result']['timings'][0] //TODO first element only?
+                    row.push(timingResult['50th'])
+                    row.push(timingResult['50th'] + ' (' + branch + ') ' + dataOfCommit.commitMeta.commitMsg)
+                    row.push(timingResult['90th'])
+                    row.push(timingResult['90th'] + ' (' + branch + ') ' + dataOfCommit.commitMeta.commitMsg)
+                    row.push(timingResult['95th'])
+                    row.push(timingResult['95th'] + ' (' + branch + ') ' + dataOfCommit.commitMeta.commitMsg)
+                    row.push(timingResult['mean'])
+                    row.push(timingResult['mean'] + ' (' + branch + ') ' + dataOfCommit.commitMeta.commitMsg)
+                }
+            } else {
+                columnPerBranchCount = chartType === ChartTypes.Simple ? 2 : 8
+                for (i = 0 ; i < columnPerBranchCount ; i++) {
+                    row.push(null)
+                }
+            }
+        })
+
+        rows.push(row)
+    })
+
+
+    // Add the rows
+    data.addRows(rows);
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.ChartWrapper({
+        chartType: 'LineChart',
+        containerId: elementId,
+        dataTable: data,
+        options: options
+    });
+
+    google.visualization.events.addListener(chart, 'select', showHideSeries)
+
+    var dataView = chart.getView() || {};
+    dataView.columns = viewColumns
+    chart.setView(dataView)
+    chart.draw();
+
+
+    function showHideSeries(target) {
+        var sel = chart.getChart().getSelection();
+        // if selection length is 0, we deselected an element
+        if (sel.length > 0) {
+            // if row is undefined, we clicked on the legend
+            if (sel[0].row == null) {
+                var selectedColIndex = sel[0].column;
+                console.log(selectedColIndex)
+                columns[selectedColIndex].visible = !columns[selectedColIndex].visible //flip the status
+                if (columns[selectedColIndex].visible) {
+                    viewColumns[selectedColIndex] = selectedColIndex
+                } else {
+                    viewColumns[selectedColIndex] = {
+                                        label: columns[selectedColIndex].label,
+                                        type: columns[selectedColIndex].type,
+                                        calc: function () {
+                                            return null;
+                                        }
+                                    };
+                }
+                dataView = chart.getView()
+                dataView.columns = viewColumns
+                chart.setView(dataView)
+                //regen colors
+                var series = []
+                $.each(columns, function(index, column) {
+                    if (column.type == 'number') {
+                        if (column.visible) {
+                            series.push({}) //use default color
+                        } else {
+                            series.push({ color : '#CCCCCC'})
+                        }
+                    }
+                })
+                chart.setOption('series', series);
+                chart.draw();
+            }
+        }
+    }
+}
+
+google.load('visualization', '1', {packages: ['corechart'], callback: drawAllCharts});
