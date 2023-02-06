948a8d2023b9feed41cb28419c87c20c2363326c
Ishan Chattopadhyaya <ishan@searchscale.com>
2023-02-06 19:03:55 +0530

    Fixing stress.sh with -g, -v params, adding validations file for stress-facets-local.json

--- backtest.sh
+++ backtest.sh
@@ -1,28 +1,25 @@
+ORIGINAL_DIR=`pwd`
 cd SolrNightlyBenchmarksWorkDirectory/Download/solr-repository
 git reset --hard; git clean -fdx
 git checkout branch_9x
 git pull
 
-#git log --since="25 August 2022" --until="29 October 2022" |head -n 25
-
-for testname in "stress-facets-local.json" "cluster-test.json"
-do
-    for commit in `git log --since="1 September 2022" --until="1 January 2029" --pretty=format:"%H"| shuf`;
-    do echo; echo "Running $commit"
-
-    if [ -f "/home/ishan/code/solr-bench/suites/results/results-$testname-$commit.json" ]; then
+for commit in `git log --since="1 July 2022" --until="1 January 2029" --pretty=format:"%H"`;
+do echo; echo "Running $commit"
+  for testnamefile in "cluster-test.json" "stress-facets-local.json"
+  do
+    cd $ORIGINAL_DIR
+    testname=`echo $testnamefile|cut -d "." -f 1`
+    if [ -f "suites/results/$testname/results-$commit.json" ]; then
         echo "Result file already exists for $commit"
     else
         echo "Trying commit: $commit"
-        /home/ishan/code/solr-bench/cleanup.sh
-        /home/ishan/code/solr-bench/stress.sh -c $commit /home/ishan/code/solr-bench/suites/$testname
-        #cd -; python createGraph.py --test $testname && cp $testname.html /var/www/html/$testname.html; cd -
-	cd -; python createBranchGraph.py -b branch_9x; cp branch*html /var/www/html; cd -
+        ./cleanup.sh
+        ./stress.sh -c $commit -v suites/$testnamefile
     fi
 
-    done
-
+  done
 done
 
-cd -
+cd $ORIGINAL_DIR
 
